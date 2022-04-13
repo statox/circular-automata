26318b45b85aa0ba2e10f1a8ccc5e8b408cd282d
Adrien Fabre <adrien.fabre@dashlane.com>
2022-04-13 14:36:18 +0200

    Add control on which parameters are randomly changed

--- src/components/GenerationControls.svelte
+++ src/components/GenerationControls.svelte
@@ -4,16 +4,31 @@
 
     export let data: AppData;
 
+    let settings = {
+        W: false,
+        H: true,
+        rule: true,
+        firstLine: true
+    };
+
     const MAX_W = 600;
     const MAX_H = 600;
 
     const generateRandom = () => {
-        data.W = Math.floor(Math.random() * MAX_W);;
-        data.H = Math.floor(Math.random() * MAX_H);
-        data.ruleNumber = Math.floor(Math.random() * 256);
-        data.firstLine = [];
-        for (let _=0; _<data.W; _++) {
-            data.firstLine.push(Math.random() > 0.7);
+        if (settings.W) {
+            data.W = Math.floor(Math.random() * MAX_W);;
+        }
+        if (settings.H) {
+            data.H = Math.floor(Math.random() * MAX_H);
+        }
+        if (settings.rule) {
+            data.ruleNumber = Math.floor(Math.random() * 256);
+        }
+        if (settings.firstLine || settings.W) {
+            data.firstLine = [];
+            for (let _=0; _<data.W; _++) {
+                data.firstLine.push(Math.random() > 0.7);
+            }
         }
         dispatch('updateData')
     }
@@ -23,9 +38,16 @@
     <div class="center controlSectionDiv">
         <h3>Generation controls</h3>
         <div>
-            <span>
-                <button on:click={generateRandom}>Generate a new random configuration</button>
-            </span>
+            <p>Select the parameters you want to change randomly</p>
+        </div>
+        <div>
+            <span>Line width <input type="checkbox" bind:checked={settings.W} id="toggleLineWidth"></span>
+            <span>Automata height <input type="checkbox" bind:checked={settings.H} id="toggleHeight"></span>
+            <span>Rule <input type="checkbox" bind:checked={settings.rule} id="toggleRule"></span>
+            <span>First line <input type="checkbox" bind:checked={settings.firstLine} id="toggleFirstLine"></span>
+        </div>
+        <div>
+            <button on:click={generateRandom}>Generate a new random configuration</button>
         </div>
     </div>
 </main>
