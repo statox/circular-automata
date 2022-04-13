44001e7ee1dc08f226a6f06e6b9f7401377d1f1a
Adrien Fabre <adrien.fabre@dashlane.com>
2022-04-13 13:51:18 +0200

    Extract random generation to its own component

--- src/components/Controls.svelte
+++ src/components/Controls.svelte
@@ -1,39 +1,11 @@
 <script lang="ts">
     import RuleControls from './RuleControls.svelte';
     import DimensionsControls from './DimensionsControls.svelte';
+    import GenerationControls from './GenerationControls.svelte';
     import { createEventDispatcher } from 'svelte'
     const dispatch = createEventDispatcher()
 
     export let data: AppData;
-
-    const MAX_W = 600;
-    const MAX_H = 600;
-
-    let randomChanges = false;
-    let randomChangesTimer;
-
-    const toggleViewType = () => {
-        data.type = data.type === 'square' ? 'circle' : 'square';
-        dispatch('updateData')
-    }
-
-    const toggleRandomChanges = () => {
-        if (randomChanges) {
-            clearInterval(randomChangesTimer);
-        } else {
-            randomChangesTimer = setInterval(() => {
-                data.W = Math.floor(Math.random() * MAX_W);;
-                data.H = Math.floor(Math.random() * MAX_H);
-                data.ruleNumber = Math.floor(Math.random() * 256);
-                data.firstLine = [];
-                for (let _=0; _<data.W; _++) {
-                    data.firstLine.push(Math.random() > 0.7);
-                }
-                dispatch('updateData')
-            }, 3000);
-        }
-        randomChanges = !randomChanges;
-    }
 
     const dataUpdatedInSubComponent = () => {
         data = data;
@@ -45,9 +17,5 @@
     <h2>Controls</h2>
     <RuleControls data={data} on:updateData={dataUpdatedInSubComponent} />
     <DimensionsControls data={data} on:updateData={dataUpdatedInSubComponent} />
-    <div>
-        <span>
-            <button on:click={toggleRandomChanges}>{randomChanges ? 'Stop' : 'Start'} random changes</button>
-        </span>
-    </div>
+    <GenerationControls data={data} on:updateData={dataUpdatedInSubComponent} />
 </main>
---
+++ src/components/GenerationControls.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+    import { createEventDispatcher } from 'svelte'
+    const dispatch = createEventDispatcher()
+
+    export let data: AppData;
+
+    const MAX_W = 600;
+    const MAX_H = 600;
+
+    const generateRandom = () => {
+        data.W = Math.floor(Math.random() * MAX_W);;
+        data.H = Math.floor(Math.random() * MAX_H);
+        data.ruleNumber = Math.floor(Math.random() * 256);
+        data.firstLine = [];
+        for (let _=0; _<data.W; _++) {
+            data.firstLine.push(Math.random() > 0.7);
+        }
+        dispatch('updateData')
+    }
+</script>
+
+<main>
+    <div class="center controlSectionDiv">
+        <h3>Generation controls</h3>
+        <div>
+            <span>
+                <button on:click={generateRandom}>Generate a new random configuration</button>
+            </span>
+        </div>
+    </div>
+</main>
+
+<style src="./Controls.css"></style>
+
