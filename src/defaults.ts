3c78904a9822ff6e9ed87a4f5be0dab7f4759c2a
Adrien Fabre <adrien.fabre@dashlane.com>
2022-04-14 22:59:04 +0200

    Refactor defaults for better developer experience

---
+++ src/defaults.ts
@@ -0,0 +1,48 @@
+import type {
+    AnimationStore,
+    AutomatonDimensionsStore,
+    ColorStore,
+    GenerationControlsStore,
+    GenerationSettingsStore
+} from './types/stores.types';
+
+const defaultAnimationStore: AnimationStore = {
+    play: true,
+    fps: 30
+};
+
+const defaultAutomatonDimensionsStore: AutomatonDimensionsStore = {
+    W: 50,
+    H: 50
+};
+
+const defaultColorStore: ColorStore = {
+    background: {H: 307, S: 87, B: 36},
+    fill: {H: 157, S: 88, B: 36},
+    firstLineFill: {H: 37, S: 88, B: 36}
+};
+
+const defaultRule = 22;
+
+const defaultDenerationControlsStore: GenerationControlsStore = {
+    enableAutomaticGeneration: false,
+    interval: 1000
+};
+
+const defaultGenerationSettingsStore: GenerationSettingsStore = {
+    W: false,
+    H: false,
+    rule: true,
+    firstLine: true,
+    color: true,
+    firstLineDensity: 50
+};
+
+export {
+    defaultAnimationStore,
+    defaultAutomatonDimensionsStore,
+    defaultColorStore,
+    defaultDenerationControlsStore,
+    defaultGenerationSettingsStore,
+    defaultRule
+};
--- src/stores.ts
+++ src/stores.ts
@@ -1,4 +1,12 @@
 import {writable} from 'svelte/store';
+import {
+    defaultAnimationStore,
+    defaultAutomatonDimensionsStore,
+    defaultColorStore,
+    defaultDenerationControlsStore,
+    defaultGenerationSettingsStore,
+    defaultRule
+} from './defaults';
 import type {Automaton} from './types/automaton.types';
 import type {
     AnimationStore,
@@ -9,40 +17,16 @@ import type {
     GenerationSettingsStore
 } from './types/stores.types';
 
-const animationStore = writable({
-    play: false,
-    fps: 20
-} as AnimationStore);
+const animationStore = writable(defaultAnimationStore);
+const automatonDimensionsStore = writable(defaultAutomatonDimensionsStore);
+const colorStore = writable(defaultColorStore);
+const generationControlsStore = writable(defaultDenerationControlsStore);
+const generationSettingsStore = writable(defaultGenerationSettingsStore);
+const rule = writable(defaultRule);
 
-const colorStore = writable({
-    background: {H: 307, S: 87, B: 36},
-    fill: {H: 157, S: 88, B: 36},
-    firstLineFill: {H: 37, S: 88, B: 36}
-} as ColorStore);
-
-const rule = writable(110);
 const automatonStore = writable({
     A: null as Automaton
 } as AutomatonStore);
-
-const automatonDimensionsStore = writable({
-    W: 50,
-    H: 50
-} as AutomatonDimensionsStore);
-
-const generationSettingsStore = writable({
-    W: false,
-    H: false,
-    rule: true,
-    firstLine: true,
-    color: true,
-    firstLineDensity: 50
-} as GenerationSettingsStore);
-
-const generationControlsStore = writable({
-    enableAutomaticGeneration: true,
-    interval: 1000
-} as GenerationControlsStore);
 
 export {
     animationStore,
