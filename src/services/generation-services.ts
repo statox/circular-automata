0ecd7af78e69fe11438376bc242a17098e7b9f0c
Adrien Fabre <adrien.fabre@dashlane.com>
2022-04-14 22:36:58 +0200

    Fix typo

--- src/services/generation-services.ts
+++ src/services/generation-services.ts
@@ -33,7 +33,7 @@ function getRandomAutomaton(params: {currentAutomaton: Automaton; generationSett
         ruleNumber = Math.floor(Math.random() * 256);
     }
 
-    if (generationSettings.firstLine || W !== firstLine.length) {
+    if (generationSettings.firstLine || W !== firstLine?.length) {
         firstLine = [];
         let atLeastOne = false;
         for (let i = 0; i < W; i++) {
